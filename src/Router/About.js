8026269e78eb79b3f5f43d60143117a00733b6d9
akulasathish <71421128+akulasathish@users.noreply.github.com>
2020-09-17 09:22:56 +0530

    updated by dev sathish
    updated by dev sathish

--- src/Router/About.js
+++ src/Router/About.js
@@ -8,7 +8,7 @@ class About extends Component {
     render() { 
         return (
             
-            <h1> TechPledge Consulting Training and Consulting</h1>
+            <h1> sathish akula Training and Consulting</h1>
          );
     }
 }
